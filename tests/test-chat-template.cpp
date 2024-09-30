71c086768c4c3b1d5af18b2ef050b9cf4932d402
Olivier Chafik <ochafik@google.com>
2024-09-30 23:45:15 +0100

    add missing license header

--- tests/test-chat-template.cpp
+++ tests/test-chat-template.cpp
@@ -1,3 +1,10 @@
+/*
+    Copyright 2024 Google LLC
+
+    Use of this source code is governed by an MIT-style
+    license that can be found in the LICENSE file or at
+    https://opensource.org/licenses/MIT.
+*/
 #include "chat-template.hpp"
 
 #include <string>
